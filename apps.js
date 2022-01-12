935e03c1ba64c2019b8c57efb26456896534ef2c
kennyLepe <96542019+kennyLepe@users.noreply.github.com>
2022-01-12 00:43:09 -0600

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,20 @@
+// Challenge 1
+
+    var year = 1992; 
+    var newYear = 2022;
+    function age(num1,num2){ 
+    var res = num2-num1; 
+    console.log(res);
+}
+age(year,newYear);
+
+
+
+
+// Challenge 2:
+var person = {
+        name: "Kennya Lepe", 
+        location: "Guadalajara", 
+        hobby: "take homeless cats and dogs"
+    }
+    console.log("Hi, my name is " + person.name + ". I live in " + person.location + " and enjoy " + person.hobby + ".");
